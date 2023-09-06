a4cc7ecd2d79048cf99a484aadf66e7d76891e06
Carlos <xandycarlos79@gmail.com>
2023-09-05 21:24:48 -0300

    Tela de solicitação de turma

--- Src/Pages/SolicitarTurma/SolicitarTurma.jsx
+++ Src/Pages/SolicitarTurma/SolicitarTurma.jsx
@@ -1,27 +1,23 @@
 import React, { useContext, useState } from "react";
-import { View, StyleSheet, Text, Image } from "react-native";
+import { View, StyleSheet, Text, Image, TextInput } from "react-native";
 import InserirTurma from "../../Componentes/InserirTurma";
 import ApiMotorista from "../../services/Api/ApiMotorista";
 import { AuthContext } from "../../services/Contexts/Contexts";
 import ApiCliente from "../../services/Api/ApiCiente";
-import TituloCadastro from "../../Componentes/Titulocadastros";
 import { useNavigation } from "@react-navigation/native";
-import { Ionicons } from '@expo/vector-icons'
-
-// import { useFonts, Montserrat_200ExtraLight } from "@expo-google-fonts/montserrat";
+import { ScrollView } from "react-native";
+import Touchable from "../../Componentes/Touchable";
+import styles from "./SolicitarTurma.modules";
 
 export default function SolicitarTurma() {
-    // const[fontsLoaded] = useFonts({
-    //     Montserrat_200ExtraLight
-    // })
 
     const navigation = useNavigation();
     const [solicitacaoenviada, setSolicitacaoenviada] = useState(false);
     const [codigo, setCodigo] = useState('');
     const { user } = useContext(AuthContext);
 
-    if(user.turma_cliente !== null){
-        navigation.navigate("TabBarScreen");
+    if (user.turma_cliente !== null) {
+        navigation.navigate("TabBarCliente");
     }
 
     const data = {
@@ -44,11 +40,11 @@ export default function SolicitarTurma() {
 
             });
 
-            if(response.status == 200){
+            if (response.status == 200) {
 
                 setSolicitacaoenviada(true);
             }
-            
+
         }
 
         catch (error) {
@@ -59,86 +55,52 @@ export default function SolicitarTurma() {
     }
 
     return (
+
         <View style={styles.container}>
+            <ScrollView style={styles.scroll}>
 
+                {
+                    solicitacaoenviada ? (
 
+                        <View>
 
-            {
-                solicitacaoenviada ? (
-                    
-                    <View>
-                        <View style={styles.SolicitacaoEnviada}>
-                        <Text style={{fontSize: 30}}>Solicitação enviada!</Text>
-                        <Ionicons name="checkmark-circle" size={30} color="green" style={{top:"1%"}} />
-                        
-                        </View>
-                        <Text style={styles.MsgAguardando}>Aguardando resposta </Text>
-                        <Text style={styles.MsgAguardando2}>do motorista...</Text>
-                        
-                        <Image source={require("../../../assets/waiting.gif")} style={styles.gifWainting}></Image>
-
-                    </View>
-                )
-                :
-                (
-                    <View>
-                            <Text style={{ fontSize: 35, alignSelf: "center", marginTop: "20%" }}>Solicitação</Text>
-
-                            <View style={styles.ViewGif}>
-                                <Image source={require("../../../assets/BusGif.gif")} style={styles.gifBus}></Image>
+                            <View style={styles.SolicitacaoEnviada}>
+                                <Image source={require('../../../assets/Ok.png')} style={styles.ok} />
+                                <Text style={{ fontSize: 30 }}>Solicitação enviada!</Text>
                             </View>
-                            <View style={styles.inserirTurma}>
-                                <InserirTurma evento={() => EnviarSolicitacao()} valor={setCodigo}></InserirTurma>
+
+                            <Image source={require('../../../assets/waiting.gif')} style={styles.gif} />
+
+
+                            <View style={styles.MsgAguardando}>
+                                <Text style={{color: 'white', textAlign: 'center', fontSize: 20}}>Aguardando a resposta do motorista...</Text>
                             </View>
 
                         </View>
                     )
-            }
+                        :
+                        (
+                            <View>
+                                <View style={styles.titulo}>
+                                    <Image source={require('../../../assets/Logo.png')} style={styles.imagem} />
+                                    <Text style={styles.textoTitulo}>Digite o código do motorista</Text>
+                                </View>
 
+                                <TextInput
+                                    placeholder={'Exemplo: #33782'}
+                                    style={styles.input}
+                                    onChangeText={setCodigo}>
+                                </TextInput>
+
+                                <View style={styles.botao}>
+                                    <Touchable texto={'Enviar'} evento={EnviarSolicitacao}></Touchable>
+                                </View>
+                            </View>
+                        )
+                }
+
+            </ScrollView>
         </View>
     )
 }
 
-const styles = StyleSheet.create({
-
-    container: {
-        flex: 1,
-        backgroundColor: "white",
-        
-    },
-
-    ViewGif: {
-        marginTop: "10%",
-        marginBottom: "10%",
-    },
-
-    gifBus: {
-        height: 150,
-        width: "60%",
-        alignSelf: 'center'
-    },
-
-    MsgAguardando:{
-        alignSelf: "center",
-        marginTop: "40%",
-        fontSize: 20,
-        // fontFamily: "Montserrat_200ExtraLight"        
-
-    },
-
-    MsgAguardando2:{
-        alignSelf: "center",
-        fontSize: 20
-    },
-
-    gifWainting:{
-        alignSelf: "center",
-        marginTop: "10%"
-    },
-
-    SolicitacaoEnviada:{
-        alignSelf: "center",
-        flexDirection: "row",
-        marginTop: "20%"
-    }
-})
---
+++ Src/Pages/SolicitarTurma/SolicitarTurma.modules.jsx
@@ -0,0 +1,81 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+
+
+    container: {
+        flex: 1,
+        backgroundColor: "white",
+
+    },
+
+    titulo: {
+        alignItems: "center",
+        alignSelf: "center",
+        flexDirection: "column",
+        justifyContent: "center",
+        marginTop: "40%"
+    },
+
+    textoTitulo: {
+        fontSize: 32,
+        textAlign: "center",
+        marginBottom: "20%"
+
+    },
+
+    imagem: {
+        top: 15,
+        alignSelf: 'center',
+        width: 130,
+        height: 100,
+        marginBottom: "5%"
+    },
+
+    input: {
+        alignSelf: "center",
+        backgroundColor: 'rgba(196, 196, 196, 0.20)',
+        height: '8%',
+        width: "80%",
+        borderRadius: 10,
+        borderColor: "#F7770D",
+        borderWidth: 0.5,
+        paddingLeft: 15,
+        placeholderTextColor: 'red'
+    },
+
+    botao: {
+        marginTop: "35%",
+        marginBottom: "10%"
+    },
+
+    MsgAguardando: {
+        alignSelf: "center",
+        backgroundColor: "#F7770D",
+        marginTop: '25%',
+        width: '95%',
+        height: '9%',
+        borderRadius: 10,
+        marginBottom: '10%',
+        justifyContent: 'center'
+        
+    },
+
+    gif: {
+        alignSelf: "center",
+        marginTop: "15%"
+    },
+
+    ok:{
+        alignSelf: "center",
+        marginBottom: "5%"
+    },
+
+    SolicitacaoEnviada: {
+        alignSelf: "center",
+        flexDirection: "column",
+        marginTop: "20%"
+    }
+})
+
+export default styles;
